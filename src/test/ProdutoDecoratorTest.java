2c0ebe0dc68db89cd51a0f410ade6684c1c59d1d
Matheus Rocha <01devmath@gmail.com>
2023-07-24 18:19:45 -0300

    test: add tests for Produto color output

--- src/test/ProdutoDecoratorTest.java
+++ src/test/ProdutoDecoratorTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import src.produto.Produto;
+import src.produto.ProdutoColorido;
 import src.produto.ProdutoDecorator;
 import src.produto.ProdutoItalico;
 import src.produto.ProdutoNegrito;
@@ -40,6 +41,17 @@ public class ProdutoDecoratorTest {
 
     assertEquals(
         "<span style=\"font-weight:bold\"><span style=\"font-style:italic\">Mouse, Eletrônicos, $25.00, 10 unidade(s) em estoque</span></span>",
+        produtoDecorado.formataParaImpressao());
+  }
+
+  @Test
+  public void testCorProdutoDecorator() {
+    Produto produto = new ProdutoPadrao(1, "Mouse", "Eletrônicos", 10, 25.0);
+    String cor = "#0000ff";
+    ProdutoDecorator produtoDecorado = new ProdutoColorido(produto, cor);
+
+    assertEquals(
+        "<span style=\"color: #0000ff\">Mouse, Eletrônicos, $25.00, 10 unidade(s) em estoque</span>",
         produtoDecorado.formataParaImpressao());
   }
 }
