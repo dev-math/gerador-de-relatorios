2bf5c91adaa49e41d76cf6b38722f5149d9145d4
Matheus Rocha <01devmath@gmail.com>
2023-07-24 13:59:15 -0300

    test: implement product decorator tests

---
+++ src/test/ProdutoDecoratorTest.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import src.produto.Produto;
+import src.produto.ProdutoDecorator;
+import src.produto.ProdutoItalico;
+import src.produto.ProdutoNegrito;
+import src.produto.ProdutoPadrao;
+
+public class ProdutoDecoratorTest {
+
+  @Test
+  public void testProdutoItalico() {
+    final Produto produto =
+        new ProdutoPadrao(1, "Mouse", "Eletrônicos", 10, 25.0);
+    final ProdutoDecorator produtoDecorado = new ProdutoItalico(produto);
+
+    assertEquals(
+        "<span style=\"font-style:italic\">Mouse, Eletrônicos, $25.00, 10 unidade(s) em estoque</span>",
+        produtoDecorado.formataParaImpressao());
+  }
+
+  @Test
+  public void testProdutoNegrito() {
+    final Produto produto =
+        new ProdutoPadrao(1, "Mouse", "Eletrônicos", 10, 25.0);
+    final ProdutoDecorator produtoDecorado = new ProdutoNegrito(produto);
+
+    assertEquals(
+        "<span style=\"font-weight:bold\">Mouse, Eletrônicos, $25.00, 10 unidade(s) em estoque</span>",
+        produtoDecorado.formataParaImpressao());
+  }
+
+  @Test
+  public void testProdutoItalicoNegrito() {
+    final Produto produto =
+        new ProdutoPadrao(1, "Mouse", "Eletrônicos", 10, 25.0);
+    final ProdutoDecorator produtoDecorado =
+        new ProdutoNegrito(new ProdutoItalico(produto));
+
+    assertEquals(
+        "<span style=\"font-weight:bold\"><span style=\"font-style:italic\">Mouse, Eletrônicos, $25.00, 10 unidade(s) em estoque</span></span>",
+        produtoDecorado.formataParaImpressao());
+  }
+}
