a39bfc0e9f3bb2b53ee0430eecb33b5667b922f3
Matheus Rocha <01devmath@gmail.com>
2023-07-24 18:20:26 -0300

    fix: dont need to use format codes anymore

--- src/produto/formatacao/FormatTypes.java
+++ src/produto/formatacao/FormatTypes.java
@@ -4,19 +4,13 @@ package src.produto.formatacao;
  * FormatTypes
  */
 public enum FormatTypes {
-  DEFAULT("padrao", 0b0000),
-  BOLD("negrito", 0b0001),
-  ITALIC("italico", 0b0010);
+  DEFAULT("padrao"),
+  BOLD("negrito"),
+  ITALIC("italico");
 
   String formatName;
-  int formatCode;
 
-  private FormatTypes(final String formatName, final int formatCode) {
-    this.formatName = formatName;
-    this.formatCode = formatCode;
-  }
-
-  public int getFormatCode() { return formatCode; }
+  private FormatTypes(final String formatName) { this.formatName = formatName; }
 
   public static FormatTypes getFormatTypeByName(final String name) {
     for (final FormatTypes type : FormatTypes.values()) {
